e12b9a590732e61ddaaeb3db9f7e59d38a5cb2a3
dashameter <dashameter@gmail.com>
2021-06-12 16:04:00 +0700

    feat: automatically add dashpay contract id to .evoenv vars

--- scripts/resetDashmate.sh
+++ scripts/resetDashmate.sh
@@ -23,15 +23,22 @@ dashmate wallet:mint 10 --address=yR5sQ16UZ3Rpni9bqueF81bsHTkJdioSgN --config=lo
 # HoneyBadger 'sphere ozone bachelor raise clutch mercy mansion cook teach eager sleep gadget'
 dashmate wallet:mint 10 --address=yTQousskoh6e2rGvThtYktzbqXc5MYuATF --config=local_seed
 
+# 'legend chase bless great skirt street member push calm real menu flee'
+dashmate wallet:mint 10 --address=yUwkuzCY9U5aLTTaEenf7jnnRK9LmXAQ2o --config=local_seed
+
 # Start dashmate
 dashmate group:start  --wait-for-readiness
 
 
+echo "" >> ~/.evoenv
 echo "export NUXT_DPNS_CONTRACT_ID=$(dashmate config:get platform.dpns.contract.id --config=local_1)" >> ~/.evoenv
 echo "export VUE_APP_DPNS_CONTRACT_ID=$(dashmate config:get platform.dpns.contract.id --config=local_1)" >> ~/.evoenv
+echo "export NUXT_DASHPAY_CONTRACT_ID=$(dashmate config:get platform.dashpay.contract.id --config=local_1)" >> ~/.evoenv
+echo "export VUE_APP_DASHPAY_CONTRACT_ID=$(dashmate config:get platform.dashpay.contract.id --config=local_1)" >> ~/.evoenv
 echo "export NUXT_DAPIADDRESSES='[\"127.0.0.1:3000\"]'" >> ~/.evoenv
 echo "export VUE_APP_DAPIADDRESSES='[\"127.0.0.1:3000\"]'" >> ~/.evoenv
 echo "export NUXT_MNEMONIC=\"rival estate inside turn journey charge window rhythm marble audit amateur bus\"" >> ~/.evoenv
+echo "" >> ~/.evoenv
 
 
 ./scripts/registerTestUser.sh
